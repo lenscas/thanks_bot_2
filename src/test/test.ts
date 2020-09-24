368cb6084ae9a3c0a2db6e8393105374fbe7e677
lenscas <lenscas@gmail.com>
2020-09-24 23:25:02 +0200

    fix lint errors

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,8 +1,8 @@
 import { Pool } from 'pg';
 import { testQuery } from './test.queries';
 
-export async function test(pool: Pool) {
-    const connection = pool.connect();
+export async function test(pool: Pool): Promise<undefined> {
     const x = await testQuery.run(undefined, pool);
     console.log(x);
+    return;
 }

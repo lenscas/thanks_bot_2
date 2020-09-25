a7e94545463fda8c06c4aa29361466ae69845923
lenscas <lenscas@gmail.com>
2020-09-25 11:42:12 +0200

    add a basic db abstraction
    prevents forgetting to release a connection
    prevents forgetting to end transactions

---
+++ src/db.ts
@@ -0,0 +1,47 @@
+import { Pool, PoolClient } from 'pg';
+
+export class ClientWrapper {
+    onTransactionError: 'rollback' | 'commit' = 'rollback';
+    onTransactionSuccess: 'rollback' | 'commit' = 'commit';
+    client: PoolClient;
+
+    constructor(client: PoolClient | ClientWrapper) {
+        if ('client' in client) {
+            this.client = client.client;
+        } else {
+            this.client = client;
+        }
+    }
+
+    newLayer = (): ClientWrapper => new ClientWrapper(this);
+
+    startTransaction = async <T>(func: (client: ClientWrapper) => Promise<T>): Promise<T> => {
+        this.onTransactionError = 'rollback';
+        this.onTransactionSuccess = 'commit';
+        await this.client.query('BEGIN');
+        try {
+            const res = await func(this);
+            await this.client.query(this.onTransactionSuccess);
+            return res;
+        } catch (e) {
+            await this.client.query(this.onTransactionError);
+            throw e;
+        }
+    };
+}
+
+export class PoolWrapper extends Pool {
+    getCon = async <T>(func: (client: ClientWrapper) => Promise<T>): Promise<T> => {
+        const connection = await this.connect();
+        const wrapper = new ClientWrapper(connection);
+        try {
+            const res = await func(wrapper);
+            return res;
+        } finally {
+            connection.release();
+        }
+    };
+    prepare = async <T>(func: (client: ClientWrapper) => Promise<T>): Promise<T> => {
+        return this.getCon((v) => v.startTransaction(func));
+    };
+}

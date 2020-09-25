edf96f4a61b2970281e5ba6443126386443e9734
lenscas <lenscas@gmail.com>
2020-09-25 14:05:17 +0200

    Improve database config loading

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import { Pool } from 'pg';
 import { test } from './test/test';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const db_config = require('../database.json') as {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    dev: any;
-};
+import db_config from '../database.json';
 
 const pool = new Pool(db_config.dev);
 

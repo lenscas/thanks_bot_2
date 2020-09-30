adff7572f6b3800152118def1108edcd5a074cfd
lenscas <lenscas@gmail.com>
2020-09-30 19:42:38 +0200

    set mute role

--- src/commands/moderators/queries.queries.ts
+++ src/commands/moderators/queries.queries.ts
@@ -61,3 +61,62 @@ const setCommandChannelIR: any = {"name":"set_command_channel","params":[{"name"
 export const setCommandChannel = new PreparedQuery<ISetCommandChannelParams,ISetCommandChannelResult>(setCommandChannelIR);
 
 
+/** 'SetMuteRole' parameters type */
+export interface ISetMuteRoleParams {
+  server_id: string | null | void;
+  role_id: string | null | void;
+}
+
+/** 'SetMuteRole' return type */
+export type ISetMuteRoleResult = void;
+
+/** 'SetMuteRole' query type */
+export interface ISetMuteRoleQuery {
+  params: ISetMuteRoleParams;
+  result: ISetMuteRoleResult;
+}
+
+const setMuteRoleIR: any = {"name":"set_mute_role","params":[{"name":"server_id","transform":{"type":"scalar"},"codeRefs":{"used":[{"a":517,"b":525,"line":17,"col":9}]}},{"name":"role_id","transform":{"type":"scalar"},"codeRefs":{"used":[{"a":529,"b":535,"line":17,"col":21},{"a":606,"b":612,"line":20,"col":22}]}}],"usedParamSet":{"server_id":true,"role_id":true},"statement":{"body":"INSERT INTO server_config (server_id, mute_role)\nVALUES (:server_id, :role_id)\nON CONFLICT ON CONSTRAINT server_config_pk\nDO\nUPDATE SET mute_role=:role_id","loc":{"a":459,"b":612,"line":16,"col":0}}};
+
+/**
+ * Query generated from SQL:
+ * ```
+ * INSERT INTO server_config (server_id, mute_role)
+ * VALUES (:server_id, :role_id)
+ * ON CONFLICT ON CONSTRAINT server_config_pk
+ * DO
+ * UPDATE SET mute_role=:role_id
+ * ```
+ */
+export const setMuteRole = new PreparedQuery<ISetMuteRoleParams,ISetMuteRoleResult>(setMuteRoleIR);
+
+
+/** 'GetMuteRole' parameters type */
+export interface IGetMuteRoleParams {
+  server_id: string | null | void;
+}
+
+/** 'GetMuteRole' return type */
+export interface IGetMuteRoleResult {
+  mute_role: string | null;
+}
+
+/** 'GetMuteRole' query type */
+export interface IGetMuteRoleQuery {
+  params: IGetMuteRoleParams;
+  result: IGetMuteRoleResult;
+}
+
+const getMuteRoleIR: any = {"name":"get_mute_role","params":[{"name":"server_id","transform":{"type":"scalar"},"codeRefs":{"used":[{"a":697,"b":705,"line":25,"col":19}]}}],"usedParamSet":{"server_id":true},"statement":{"body":"SELECT mute_role\nFROM server_config\nWHERE server_id = :server_id","loc":{"a":642,"b":705,"line":23,"col":0}}};
+
+/**
+ * Query generated from SQL:
+ * ```
+ * SELECT mute_role
+ * FROM server_config
+ * WHERE server_id = :server_id
+ * ```
+ */
+export const getMuteRole = new PreparedQuery<IGetMuteRoleParams,IGetMuteRoleResult>(getMuteRoleIR);
+
+
--- src/commands/moderators/queries.sql
+++ src/commands/moderators/queries.sql
@@ -11,3 +11,15 @@ VALUES (:server_id, :channel_id)
 ON CONFLICT ON CONSTRAINT server_config_pk
 DO
 UPDATE SET command_channel=:channel_id;
+
+/* @name set_mute_role */
+INSERT INTO server_config (server_id, mute_role)
+VALUES (:server_id, :role_id)
+ON CONFLICT ON CONSTRAINT server_config_pk
+DO
+UPDATE SET mute_role=:role_id;
+
+/* @name get_mute_role */
+SELECT mute_role
+FROM server_config
+WHERE server_id = :server_id;

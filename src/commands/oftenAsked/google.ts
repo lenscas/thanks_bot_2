12487884109e000b531da6db9b66ce0f57c333bc
Harsh Singh <harshhh.singh.personal@gmail.com>
2020-10-05 18:33:40 -0400

    Fixed space to "+"

--- src/commands/oftenAsked/google.ts
+++ src/commands/oftenAsked/google.ts
@@ -2,7 +2,7 @@ import { create_command } from '../../command';
 
 export const command = create_command(
     async ({ message, args }) => {
-        let search = args.join(' ')
+        let search = args.join('+')
 
         await message.channel.send(`Google might have the answer to that for you! https://www.google.com/search?q=${search}`);
     },

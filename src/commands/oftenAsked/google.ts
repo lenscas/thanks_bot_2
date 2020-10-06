59a785edd04e1dfa7df214dff538cfb1171a2965
Harsh Singh <harshhh.singh.personal@gmail.com>
2020-10-06 09:05:14 -0400

    ran yarn lint fix.

--- src/commands/oftenAsked/google.ts
+++ src/commands/oftenAsked/google.ts
@@ -2,9 +2,11 @@ import { create_command } from '../../command';
 
 export const command = create_command(
     async ({ message, args }) => {
-        let search = args.join('+')
+        const search = args.join('+');
 
-        await message.channel.send(`Google might have the answer to that for you! https://www.google.com/search?q=${search}`);
+        await message.channel.send(
+            `Google might have the answer to that for you! https://www.google.com/search?q=${search}`,
+        );
     },
     'Gives Google search results for the question.',
     ['ask', 'search'],

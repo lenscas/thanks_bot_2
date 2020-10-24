22b2b025592942ac73bf27862d1d595dd2cedccc
Megarev <64357296+Megarev@users.noreply.github.com>
2020-10-24 12:02:35 +0300

    Fixed codeblock command
    Removed space between the starting backticks and language
    removed extra backtick at end
    added example on how it looks

--- src/commands/oftenAsked/codeblock.ts
+++ src/commands/oftenAsked/codeblock.ts
@@ -5,9 +5,12 @@ export const command = create_command(
         await message.channel.send([
             '**Use Codeblocks To Paste Your Code**',
             'If the code is larger than 2,000 characters, then consider using a service such as <https://paste.myst.rs/>',
-            '\\``` scripting language',
+            '\\```scripting language',
             '    //your code here',
-            '\\````',
+            '\\```',
+            '```',
+            '//your code here',
+            '```',
         ]);
     },
     'Explains how to paste code neatly',

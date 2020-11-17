18a3558f6656e9e66f694f7fb6666e95a0ef400a
lenscas <lenscas@gmail.com>
2020-11-17 09:35:03 +0100

    add test server to links

--- src/commands/bot/links.ts
+++ src/commands/bot/links.ts
@@ -4,6 +4,7 @@ export const command = create_command_for_command_channel(async ({ message }) =>
     await message.channel.send([
         'My code is at <https://github.com/lenscas/thanks_bot_2>',
         'Bugs reports and feature requests can be asked at: <https://github.com/lenscas/thanks_bot_2/issues>',
+        'There is a test server you can join using: https://discord.gg/g2NaNcS',
         'Thanks for showing interest in my code and have a nice day :)',
     ]);
 }, 'Puts some links in the chat related to the code of thanks_bot');

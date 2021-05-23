0f19f9216a6c1990712a7b4e7668bb25100a820e
lenscas <lenscas@gmail.com>
2021-05-23 18:21:44 +0200

    remove command cooldown it was disabled anyway

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Client } from 'discord.js';
 
 import db_config from '../database.json';
-import { commandPrefix, discordToken, cooldown } from '../config.json';
+import { commandPrefix, discordToken } from '../config.json';
 import { get_commands_in, find_command } from './command';
 import path from 'path';
 import { help } from './help';
@@ -14,7 +14,6 @@ import { checkSpam } from './spamProtection';
 const client = new Client();
 
 (async () => {
-    const cooldowns = new Set<string>();
     const commands = await get_commands_in(path.join(__dirname, 'commands'));
     const db = new PoolWrapper(db_config.dev);
 
@@ -27,15 +26,6 @@ const client = new Client();
                 return;
             }
             if (!message.content.startsWith(commandPrefix)) return;
-
-            console.log(cooldowns);
-            if (cooldowns.has(message.author.id)) return;
-
-            cooldowns.add(message.author.id);
-            setTimeout(() => {
-                console.log('run delete');
-                cooldowns.delete(message.author.id);
-            }, cooldown * 10000 + 1);
 
             const messageArray = (message.content.split('\n')[0] ?? '').split(' ');
 
